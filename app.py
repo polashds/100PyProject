d16e3a14e4c2e1799dc348b3656bfc7822f9e56f
polashds <polashtechai@gmail.com>
2025-01-01 21:21:15 +0600

    exercise started

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+#Exercise-1: print code horizonatally
+#annotations: Print function has end parameter, it has escape character(end="\n"), 
+#remove the escape character and replace it with empty string (end="")
+
+list1 = [1, 2, 3, 4]
+for number in list1:
+    print(number, end="")
+
+# print function has another parameter sep, used to separate output
+print('02', '02', '1988', sep='/')
+
+#Exercise-2: Merging Dictionaries
+#note: use merge(I) operator or the (**) operator
+name1={"polo:": 39, 
+       "wadia":4}
+name2={"tania":35}
+#mehtod1
+names=name1|name2
+print(names)
+
+#mehtod2
+names={**name1, **name2}
+print(names)
